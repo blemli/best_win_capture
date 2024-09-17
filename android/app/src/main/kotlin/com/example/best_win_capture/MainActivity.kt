53e1a7e583b92b5ff460259bcde0bfb5efe85d9f
grafst <grafst@users.noreply.github.com>
2024-09-17 07:45:42 +0200

    start all over again

---
+++ android/app/src/main/kotlin/com/example/best_win_capture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.best_win_capture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/best_win_capture.dart
+++
@@ -1,8 +0,0 @@
-
-import 'best_win_capture_platform_interface.dart';
-
-class BestWinCapture {
-  Future<String?> getPlatformVersion() {
-    return BestWinCapturePlatform.instance.getPlatformVersion();
-  }
-}
